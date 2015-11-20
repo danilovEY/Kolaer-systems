c60d0b23baac105eb222f69e03c3f1ddf7252050
Danilov Eugene <daniloveugene@yandex.ru>
2015-11-20 17:39:49 +0300

    Add argv --root_set. If root_set=root user getting access for edit.

--- src/main/java/ru/kolaer/asmc/runnable/JavaFXLauncher.java
+++ src/main/java/ru/kolaer/asmc/runnable/JavaFXLauncher.java
@@ -4,12 +4,20 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import ru.kolaer.asmc.tools.SettingSingleton;
 import ru.kolaer.asmc.ui.javafx.controller.CMainFrame;
 
 public class JavaFXLauncher extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
+		if(!this.getParameters().getNamed().isEmpty()) {
+			String passRoot = this.getParameters().getNamed().get("root_set");
+			if(SettingSingleton.getInstance().getRootPass().equals(passRoot)){
+				SettingSingleton.getInstance().setRoot(true);
+			}
+		}
+		
 		new CMainFrame().start(primaryStage);
 	}
 
