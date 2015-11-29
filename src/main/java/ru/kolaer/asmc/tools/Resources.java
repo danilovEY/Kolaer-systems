71eee9595ad0185a401892ff19507b20fc6aff57
Danilov Eugene <daniloveugene@yandex.ru>
2015-11-29 15:43:18 +0300

    Change version to 3.0.

--- src/main/java/ru/kolaer/asmc/tools/Resources.java
+++ src/main/java/ru/kolaer/asmc/tools/Resources.java
@@ -27,7 +27,7 @@ public interface Resources
 	/**Фон.*/
 	public static final String BACKGROUND_IMAGE = "resources/geometric-background.png";
 	/**Версия*/
-	public static final String VERSION = "1.0";	
+	public static final String VERSION = "3.0";	
 	
 	public static final String DIALOG_LOADING_COMPONENTS = "Загрузка компонентов";
 	
