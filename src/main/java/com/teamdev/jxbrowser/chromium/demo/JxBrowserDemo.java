63d8b10d7de69cbc2a1356b6d30ae4fb8895b225
Danilov Eugene <daniloveugene@yandex.ru>
2015-11-29 09:56:58 +0300

    Remove jxBrowser Demo.

--- src/main/java/com/teamdev/jxbrowser/chromium/demo/JxBrowserDemo.java
+++ src/main/java/com/teamdev/jxbrowser/chromium/demo/JxBrowserDemo.java
@@ -1,28 +1,25 @@
-/*
- * Copyright (c) 2000-2015 TeamDev Ltd. All rights reserved.
- * TeamDev PROPRIETARY and CONFIDENTIAL.
- * Use is subject to license terms.
- */
-
 package com.teamdev.jxbrowser.chromium.demo;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
 
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import ru.kolaer.asmc.tools.Resources;
 
 public class JxBrowserDemo{
 	
-	//private final Stage dialog = new Stage();
-    //private final Scene scene;
-   // private final Browser browser = new Browser();
+	private final Stage dialog = new Stage();
+    private final Scene scene;
+    private final Browser browser = new Browser();
     
     public JxBrowserDemo() {  
-       /* BrowserView browserView = new BrowserView(browser);
+    	BrowserView browserView = new BrowserView(browser);
 
         browser.addTitleListener(event -> {
 			Platform.runLater(() -> {
@@ -32,16 +29,15 @@ public class JxBrowserDemo{
         
         StackPane pane = new StackPane();
         pane.getChildren().add(browserView);
-        this.scene = new Scene(pane, 700, 500);*/
+        this.scene = new Scene(pane, 700, 500);
 	}
     
     public void load(String url) {
-    	initAndDisplayUI(url);
-    	//browser.loadURL(url);
+    	browser.loadURL(url);
     }
     
     public void show() {
-		/*this.dialog.setTitle("Загрузка...");
+		this.dialog.setTitle("Загрузка...");
 		this.dialog.setScene(this.scene);
 		this.dialog.setMaximized(true);
 		this.dialog.centerOnScreen();
@@ -60,43 +56,6 @@ public class JxBrowserDemo{
 			this.dialog.close();
 		});
 		
-		this.dialog.show();*/
-	}
-
-    private static void initAndDisplayUI(String url) {
-        final TabbedPane tabbedPane = new TabbedPane();
-        insertTab(tabbedPane, TabFactory.createTab(url));
-        insertNewTabButton(tabbedPane);
-
-        JFrame frame = new JFrame("JxBrowser Demo");
-        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                tabbedPane.disposeAllTabs();
-            }
-        });
-        frame.add(tabbedPane, BorderLayout.CENTER);
-        frame.setSize(1024, 768);
-        frame.setLocationRelativeTo(null);
-        frame.setIconImage(com.teamdev.jxbrowser.chromium.demo.resources.Resources.getIcon("jxbrowser16x16.png").getImage());
-        frame.setVisible(true);
-    }
-
-    private static void insertNewTabButton(final TabbedPane tabbedPane) {
-        TabButton button = new TabButton(com.teamdev.jxbrowser.chromium.demo.resources.Resources.getIcon("new-tab.png"), "New tab");
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(final ActionEvent e) {
-                insertTab(tabbedPane, TabFactory.createTab());
-            }
-        });
-        tabbedPane.addTabButton(button);
-    }
-
-    private static void insertTab(TabbedPane tabbedPane, Tab tab) {
-        tabbedPane.addTab(tab);
-        tabbedPane.selectTab(tab);
-    }
-    
-    
+		this.dialog.show();
+	}    
 }
