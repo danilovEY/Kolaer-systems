3022864b08c4792a07b66fc5d5249b2df2df0b56
Danilov Eugene <daniloveugene@yandex.ru>
2015-12-09 19:51:36 +0300

    Initialization.

---
+++ src/main/java/ru/kolaer/ru/kolaer/client/javajx/App.java
@@ -0,0 +1,17 @@
+package ru.kolaer.ru.kolaer.client.javajx;
+
+import org.springframework.web.client.RestTemplate;
+
+public class App {
+
+	public static void main(String args[]) {
+		RestTemplate restTemplate = new RestTemplate();
+
+		String url = "http://localhost:8080/kolaer/api/";
+
+		String beans = restTemplate.getForObject(url, String.class);
+		System.out.println("The object received from REST call : " + beans);
+
+	}
+
+}
