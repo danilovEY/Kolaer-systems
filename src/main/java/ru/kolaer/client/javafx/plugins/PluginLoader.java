6a53d1e158590b719facc37d2dd8651b8737205e
Danilov Eugene <daniloveugene@yandex.ru>
2015-12-12 00:02:51 +0300

    Refactoring plugins loader.

--- src/main/java/ru/kolaer/client/javafx/plugins/PluginLoader.java
+++ src/main/java/ru/kolaer/client/javafx/plugins/PluginLoader.java
@@ -9,6 +9,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -32,57 +37,93 @@ public class PluginLoader {
 		}
 
 		final List<IKolaerPlugin> result = new ArrayList<>();
-		for(final File jarFile : dirToPlugins.listFiles((File dir, String name) -> {
+
+		File[] jarFiles = dirToPlugins.listFiles((File dir, String name) -> {
 			return name.endsWith(".jar") ? true : false;
-		})){
+		});
 
-			URL jarURL = null;
-			try{
-				jarURL = jarFile.toURI().toURL();
-			}
-			catch(MalformedURLException e1){
-				LOG.error("Невозможно преобразовать в URL файл " + jarFile.getAbsolutePath() + "!", e1);
-			}
+		ExecutorService threadPoolForPlugins = null;
+		List<Future<IKolaerPlugin>> resultInThreads = new ArrayList<>();
+		if(jarFiles.length > 0){
+			threadPoolForPlugins = Executors.newFixedThreadPool(jarFiles.length);
+		}
 
-			try(final URLClassLoader classLoader = new URLClassLoader(new URL[] {
-			        jarURL }); final JarFile jarFileRead = new JarFile(jarFile);){
+		for(final File jarFile : jarFiles){
+			Future<IKolaerPlugin> resultThread = threadPoolForPlugins.submit(() -> {
+				Thread.currentThread().setName("Поток для файла: " + jarFile.getName());
+				URL jarURL = null;
+				try{
+					jarURL = jarFile.toURI().toURL();
+				}
+				catch(MalformedURLException e1){
+					LOG.error("Невозможно преобразовать в URL файл " + jarFile.getAbsolutePath() + "!", e1);
+				}
 
-				final Enumeration<?> e = jarFileRead.entries();
+				try(final URLClassLoader classLoader = new URLClassLoader(new URL[] {
+			            jarURL }); final JarFile jarFileRead = new JarFile(jarFile);){
 
-				String packageName = "";
+					final Enumeration<?> e = jarFileRead.entries();
 
-				while(e.hasMoreElements()){
-					final JarEntry je = (JarEntry) e.nextElement();
+					String packageName = "";
 
-					if(je.isDirectory() || !je.getName().endsWith(".class")){
-						continue;
-					}
+					while(e.hasMoreElements()){
+						final JarEntry je = (JarEntry) e.nextElement();
 
-					String className = je.getName().substring(0, je.getName().length() - 6);
-					className = className.replace('/', '.');
-					if(className.startsWith(packageName)){
-						try{
-							final Class<?> cls = classLoader.loadClass(className);
-							if(cls.getAnnotation(ApplicationPlugin.class) != null){
-								final IKolaerPlugin app = (IKolaerPlugin) cls.newInstance();
-								LOG.info("Добавлен плагин: {}", app.getName());
-								result.add(app);
-							}
-						}
-						catch(Throwable ex){
-							LOG.error("Невозможно прочитать класс:" + className, ex);
+						if(je.isDirectory() || !je.getName().endsWith(".class")){
 							continue;
+						}
+
+						String className = je.getName().substring(0, je.getName().length() - 6);
+						className = className.replace('/', '.');
+						if(className.startsWith(packageName)){
+							try{
+								final Class<?> cls = classLoader.loadClass(className);
+								if(cls.getAnnotation(ApplicationPlugin.class) != null){
+									final IKolaerPlugin app = (IKolaerPlugin) cls.newInstance();
+									return app;
+								}
+							}
+							catch(Throwable ex){
+								LOG.error("Невозможно прочитать класс:" + className, ex);
+								continue;
+							}
 						}
 					}
 				}
+				catch(SecurityException secEx){
+					LOG.error("Невозможно получить доступ к файлу " + jarFile.getAbsolutePath() + "!", secEx);
+				}
+				catch(IOException ioEx){
+					LOG.error("Невозможно получить доступ к файлу " + jarFile.getAbsolutePath() + "!", ioEx);
+				}
+				return null;
+			});
+
+			resultInThreads.add(resultThread);
+		}
+
+		threadPoolForPlugins.shutdown();
+
+		try{
+			if(!threadPoolForPlugins.awaitTermination(1, TimeUnit.MINUTES)){
+				LOG.error("Потоки прерваны. Истекло время ожидания!");
+				int countThreads = threadPoolForPlugins.shutdownNow().size();
+				if(countThreads > 0)
+					LOG.error("Не получено результатов с {} потоков.",countThreads);
 			}
-			catch(SecurityException secEx){
-				LOG.error("Невозможно получить доступ к файлу " + jarFile.getAbsolutePath() + "!", secEx);
-				continue;
-			}
-			catch(IOException ioEx){
-				LOG.error("Невозможно получить доступ к файлу " + jarFile.getAbsolutePath() + "!", ioEx);
-				continue;
+		}
+		catch(InterruptedException e){
+			LOG.warn("Потоки прерваны!", e);
+		}
+		finally{
+			for(Future<IKolaerPlugin> future : resultInThreads){
+				try{
+					IKolaerPlugin app = future.get();
+					result.add(app);
+				}
+				catch(InterruptedException | ExecutionException e){
+					LOG.error("Ошибка при получении результата!", e);
+				}
 			}
 		}
 		return result;
