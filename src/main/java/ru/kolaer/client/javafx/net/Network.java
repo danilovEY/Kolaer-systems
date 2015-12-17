6fa3470c3cb876af7be9d733ab9970e521ba128f
Danilov Eugene <daniloveugene@yandex.ru>
2015-12-17 13:52:08 +0300

    Remove test class.

--- src/main/java/ru/kolaer/client/javafx/net/Network.java
+++
@@ -1,8 +0,0 @@
-package ru.kolaer.client.javafx.net;
-
-import ru.kolaer.client.javafx.mvp.viewmodel.VMExplorer;
-
-public interface Network extends Runnable {
-	void registerExplorerListener(VMExplorer explorer);
-	VMExplorer getRegisterExplorerListener();
-}
