db9936fb4be97279ab19fc5319512c9714121abf
Danilov Eugene <daniloveugene@yandex.ru>
2016-01-20 08:55:19 +0300

    Add context menu for opening content tab on window.

--- src/main/java/ru/kolaer/client/javafx/mvp/view/impl/VTabImpl.java
+++ src/main/java/ru/kolaer/client/javafx/mvp/view/impl/VTabImpl.java
@@ -4,8 +4,15 @@ import java.net.URLClassLoader;
 import java.util.Optional;
 
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import ru.kolaer.client.javafx.mvp.view.VTab;
 import ru.kolaer.client.javafx.plugins.IApplication;
 
@@ -13,6 +20,7 @@ public class VTabImpl implements VTab {
 	
 	private final Tab tab;
 	private final IApplication app;
+	private Stage stage;
 	
 	public VTabImpl(final URLClassLoader loader, final IApplication app) {
 		this.tab = new Tab();
@@ -28,6 +36,28 @@ public class VTabImpl implements VTab {
 	private void init() {
 		this.tab.setText(Optional.ofNullable(this.app.getName()).orElse("Плагин"));
 		this.tab.setContent(this.app.getContent());
+		
+		final ContextMenu contextMenu = new ContextMenu();
+		final MenuItem openInWinodow = new MenuItem("Открыть в новом окне");
+		openInWinodow.setOnAction(e -> {
+			this.stage = new Stage();
+			final BorderPane contentPane = new BorderPane(this.tab.getContent());		
+			this.tab.setContent(null);
+			this.stage.setOnCloseRequest(new EventHandler<WindowEvent>() {			
+				@Override
+				public void handle(WindowEvent event) {
+					stage.setScene(null);
+					tab.setContent(contentPane.getCenter());
+				}
+			});
+			this.stage.setScene(new Scene(contentPane, 1024, 768));
+			this.stage.centerOnScreen();
+			this.stage.show();
+		});
+		
+		contextMenu.getItems().add(openInWinodow);
+		
+		this.tab.setContextMenu(contextMenu);
 	}
 
 	@Override
@@ -47,6 +77,9 @@ public class VTabImpl implements VTab {
 		Platform.runLater(() -> {
 			this.setContent(null);
 			this.tab.getTabPane().getTabs().remove(this.tab);
+			if(this.stage != null) {
+				this.stage.close();
+			}
 		});
 	}
 	
