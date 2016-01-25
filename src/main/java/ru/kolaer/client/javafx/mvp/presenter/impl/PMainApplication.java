8212871c3b4379fe26b7683e1201dc378cd6611d
Danilov Eugene <daniloveugene@yandex.ru>
2016-01-25 13:12:36 +0300

    Add adapters for US.

--- src/main/java/ru/kolaer/client/javafx/mvp/presenter/impl/PMainApplication.java
+++ src/main/java/ru/kolaer/client/javafx/mvp/presenter/impl/PMainApplication.java
@@ -1,38 +1,19 @@
 package ru.kolaer.client.javafx.mvp.presenter.impl;
 
-import java.util.List;
-
-import javafx.scene.layout.Pane;
 import ru.kolaer.client.javafx.plugins.UniformSystemApplication;
-import ru.kolaer.client.javafx.plugins.UniformSystemPlugin;
-import ru.kolaer.client.javafx.services.Service;
-import ru.kolaer.client.javafx.system.UniformSystemEditorKit;
-import ru.kolaer.client.javafx.plugins.UniformSystemLabel;
+import ru.kolaer.client.javafx.plugins.UniformSystemApplicationAdapter;
+import ru.kolaer.client.javafx.plugins.UniformSystemPluginAdapter;
 
-public class PMainApplication implements UniformSystemPlugin {
+public class PMainApplication extends UniformSystemPluginAdapter {
 
 	@Override
 	public String getName() {
 		return "Main";
 	}
-
-	@Override
-	public UniformSystemLabel getLabel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	@Override
 	public UniformSystemApplication getApplication() {
-		// TODO Auto-generated method stub
-		return new UniformSystemApplication() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				
-			}
-			
+		return new UniformSystemApplicationAdapter() {
 			@Override
 			public void stop() {
 				System.exit(-1);
@@ -43,31 +24,6 @@ public class PMainApplication implements UniformSystemPlugin {
 				// TODO Auto-generated method stub
 				return "Main";
 			}
-			
-			@Override
-			public String getIcon() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Pane getContent() {
-				// TODO Auto-generated method stub
-				return null;
-			}
 		};
 	}
-
-	@Override
-	public void initialization(UniformSystemEditorKit editorKid) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<Service> getServices() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
