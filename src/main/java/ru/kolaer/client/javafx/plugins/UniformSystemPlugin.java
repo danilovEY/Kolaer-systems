d1013d0ed5dcb3412928ad2c5558858544480939
Danilov Eugene <daniloveugene@yandex.ru>
2016-01-25 11:15:27 +0300

    Add US system ui and network api.

--- src/main/java/ru/kolaer/client/javafx/plugins/UniformSystemPlugin.java
+++ src/main/java/ru/kolaer/client/javafx/plugins/UniformSystemPlugin.java
@@ -1,7 +1,10 @@
 package ru.kolaer.client.javafx.plugins;
 
+import ru.kolaer.client.javafx.system.UniformSystemEditorKit;
+
 @UniformSystem
 public interface UniformSystemPlugin {
+	void setUniformSystemEditorKit(UniformSystemEditorKit editorKid);
 	String getName();
 	UniformSystemLabel getLabel();
 	UniformSystemApplication getApplication();
