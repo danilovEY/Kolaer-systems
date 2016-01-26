8fe3ac86089d28b3ff2befcd37a7b99520add928
Danilov Eugene <daniloveugene@yandex.ru>
2016-01-26 14:37:13 +0300

    Fix notification.

--- src/main/java/ru/kolaer/client/javafx/system/NotificationUSImpl.java
+++ src/main/java/ru/kolaer/client/javafx/system/NotificationUSImpl.java
@@ -2,22 +2,28 @@ package ru.kolaer.client.javafx.system;
 
 import org.controlsfx.control.Notifications;
 
+import javafx.application.Platform;
+
 public class NotificationUSImpl implements NotificationUS {
 
 	@Override
 	public void showSimpleNotify(final String title, final String text) {
-		final Notifications notify = Notifications.create();
-		notify.title(title);
-		notify.text(text);
-		notify.show();
+		Platform.runLater(() -> {
+			final Notifications notify = Notifications.create();
+			notify.title(title);
+			notify.text(text);
+			notify.show();
+		});
 	}
 
 	@Override
 	public void showErrorNotify(String title, String text) {
-		final Notifications notify = Notifications.create();
-		notify.title(title);
-		notify.text(text);
-		notify.showError();
+		Platform.runLater(() -> {
+			final Notifications notify = Notifications.create();
+			notify.title(title);
+			notify.text(text);
+			notify.showError();
+		});
 	}
 
 }
