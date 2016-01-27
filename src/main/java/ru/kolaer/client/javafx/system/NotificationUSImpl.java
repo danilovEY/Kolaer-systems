be899d8b78f7d0e450972a87585be315ecfcab45
Danilov Eugene <daniloveugene@yandex.ru>
2016-01-27 12:34:30 +0300

    Add set dalay for notification.

--- src/main/java/ru/kolaer/client/javafx/system/NotificationUSImpl.java
+++ src/main/java/ru/kolaer/client/javafx/system/NotificationUSImpl.java
@@ -17,6 +17,17 @@ public class NotificationUSImpl implements NotificationUS {
 			notify.show();
 		});
 	}
+	
+	@Override
+	public void showSimpleNotify(final String title, final String text, Duration duration) {
+		Platform.runLater(() -> {
+			final Notifications notify = Notifications.create();
+			notify.hideAfter(duration);
+			notify.title(title);
+			notify.text(text);
+			notify.show();
+		});
+	}
 
 	@Override
 	public void showErrorNotify(String title, String text) {
