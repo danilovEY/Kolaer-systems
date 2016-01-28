82464e5c9fe16bfed9e7bc509a013eed6cb921bc
Danilov Eugene <daniloveugene@yandex.ru>
2016-01-28 13:23:42 +0300

    Comment ASMCApplication class.

--- src/main/java/ru/kolaer/asmc/runnable/ASMCApplication.java
+++ src/main/java/ru/kolaer/asmc/runnable/ASMCApplication.java
@@ -14,9 +14,18 @@ import ru.kolaer.asmc.tools.Resources;
 import ru.kolaer.asmc.tools.SettingSingleton;
 import ru.kolaer.client.javafx.plugins.UniformSystemApplication;
 
+/**
+ * Реализация приложения для единой системы КолАЭР.
+ * 
+ * @author danilovey
+ * @version 0.1
+ */
 public class ASMCApplication implements UniformSystemApplication {
+	// Панель с контентом плагина.
 	private final BorderPane root = new BorderPane();
+	// Панель с .fxml-контента главного окна.
 	private AnchorPane pane;
+
 	@Override
 	public String getIcon() {
 		return "resources/aerIcon.png";
@@ -37,27 +46,26 @@ public class ASMCApplication implements UniformSystemApplication {
 
 	}
 
-
 	@Override
 	public void run() throws Exception {
-		if(this.pane == null) {
-			SettingSingleton.initialization();		
-				try(final InputStream stream = Resources.V_MAIN_FRAME.openStream()){
-					FXMLLoader loader = new FXMLLoader();
-					this.pane = loader.load(stream);
-					final InputStream inputStream = this.getClass().getResourceAsStream("/resources/CSS/Default/Default.css");
-					final URL inputStreamUrl = this.getClass().getResource("/resources/CSS/Default/Default.css");
-					this.pane.getStylesheets().addAll(inputStreamUrl.toExternalForm());
-					inputStream.close();
-					Platform.runLater(() -> {
-						root.setCenter(pane);
-						root.setPrefSize(800, 600);
-					});
-				}catch(MalformedURLException e){
-					throw e;
-				}catch(IOException e){
-					throw e;
-				}	
+		if(this.pane == null){
+			SettingSingleton.initialization();
+			try(final InputStream stream = Resources.V_MAIN_FRAME.openStream()){
+				FXMLLoader loader = new FXMLLoader();
+				this.pane = loader.load(stream);
+				final InputStream inputStream = this.getClass().getResourceAsStream("/resources/CSS/Default/Default.css");
+				final URL inputStreamUrl = this.getClass().getResource("/resources/CSS/Default/Default.css");
+				this.pane.getStylesheets().addAll(inputStreamUrl.toExternalForm());
+				inputStream.close();
+				Platform.runLater(() -> {
+					root.setCenter(pane);
+					root.setPrefSize(800, 600);
+				});
+			}catch(MalformedURLException e){
+				throw e;
+			}catch(IOException e){
+				throw e;
+			}
 		}
 	}
 }
