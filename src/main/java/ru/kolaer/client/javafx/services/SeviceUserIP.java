27d9d058a5f15ba12492e46985caf1f55b209497
Danilov Eugene <daniloveugene@yandex.ru>
2016-01-29 09:08:21 +0300

    Add user IP service.

---
+++ src/main/java/ru/kolaer/client/javafx/services/SeviceUserIP.java
@@ -0,0 +1,60 @@
+package ru.kolaer.client.javafx.services;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import ru.kolaer.client.javafx.tools.Resources;
+
+public class SeviceUserIP implements Service {
+	private final Logger LOG = LoggerFactory.getLogger(SeviceUserIP.class);
+	private final String username = System.getProperty("user.name");
+	private boolean isRun = false;
+	private final RestTemplate rest = new RestTemplate();
+	
+	@Override
+	public void run() {
+		while(this.isRun) {
+			final StringBuilder url = new StringBuilder("http://").append(Resources.URL_TO_KOLAER_RESTFUL.toString())
+					.append("/system/user/")
+					.append(this.username)
+					.append("/ip");
+			try {
+				final InetAddress inet = InetAddress.getLocalHost();
+				rest.postForObject(url.toString(), inet.getHostAddress(), String.class);
+				TimeUnit.HOURS.sleep(2);
+			} catch(final RestClientException ex) {
+				LOG.error("URL: \"{}\" не доступен!", url.toString());
+			} catch(final UnknownHostException e) {
+				LOG.error("Ошибка при получении IP!", e);
+			} catch(InterruptedException e) {
+				LOG.error("Прерыване задержки потока!");
+			}
+		}
+	}
+
+	@Override
+	public void setRunningStatus(final boolean isRun) {
+		this.isRun = isRun;
+	}
+
+	@Override
+	public boolean isRunning() {
+		return this.isRun;
+	}
+
+	@Override
+	public String getName() {
+		return "Передача IP";
+	}
+
+	@Override
+	public void stop() {
+		this.isRun = false;
+	}
+}
