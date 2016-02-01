ad555153ffce72df398cb31115949c77eed50674
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-01 10:29:45 +0300

    Add style to loading dialog.

--- src/main/java/ru/kolaer/client/javafx/system/DialogUSImpl.java
+++ src/main/java/ru/kolaer/client/javafx/system/DialogUSImpl.java
@@ -1,13 +1,18 @@
 package ru.kolaer.client.javafx.system;
 
 import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class DialogUSImpl implements DialogUS {
 
@@ -35,10 +40,28 @@ public class DialogUSImpl implements DialogUS {
 		final ProgressBar progress = new DefaultProgressBar();
 		Platform.runLater(() -> {
 			final Stage stage = new Stage();
+			stage.initStyle(StageStyle.UNDECORATED);
+			stage.setResizable(false);
 			final Label textLabel = new Label(text);
+			textLabel.setStyle("-fx-font-size: 17pt;");
+			textLabel.setAlignment(Pos.CENTER);
 			final ProgressIndicator pi = new ProgressIndicator(ProgressIndicator.INDETERMINATE_PROGRESS);
-			final FlowPane mainPane = new FlowPane(textLabel, pi);		
-			stage.setScene(new Scene(mainPane, 200, 100));
+			final HBox mainPane = new HBox(textLabel, pi);	
+			mainPane.setAlignment(Pos.CENTER);
+			mainPane.setPadding(new Insets(20, 20, 20, 20));
+			mainPane.setSpacing(20);
+			mainPane.setStyle(
+	                "-fx-padding: 5; " + 
+	                "-fx-border-width:5; " +
+	                "-fx-border-color: " +
+	                    "linear-gradient(" +
+	                        "to bottom, " +
+	                        "#0000FF, " +
+	                        "derive(#0099FF, 50%)" +
+	                    ");"
+	        );
+			mainPane.setEffect(new DropShadow());
+			stage.setScene(new Scene(mainPane));
 			stage.showAndWait();
 		});
 		return progress;
