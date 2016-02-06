a3ff19137b471d086196ab096b0f08500ae87360
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-07 00:02:10 +0300

    Add comment.

--- src/main/java/ru/kolaer/client/javafx/mvp/presenter/PTab.java
+++ src/main/java/ru/kolaer/client/javafx/mvp/presenter/PTab.java
@@ -1,16 +1,26 @@
 package ru.kolaer.client.javafx.mvp.presenter;
 
 import ru.kolaer.client.javafx.mvp.view.VTab;
-import ru.kolaer.client.javafx.plugins.UniformSystemApplication;
 import ru.kolaer.client.javafx.plugins.UniformSystemPlugin;
 
+/**
+ * Presenter для вкладки с контентом плагина.
+ * @author Danilov
+ * @version 0.1
+ */
 public interface PTab {
+	/**Получить модель.*/
+	UniformSystemPlugin getModel();
+	
+	/**Получить view вкладки.*/
 	VTab getView();
+	/**Задать view вкладки.*/
 	void setView(VTab tab);
 	
+	/**Активировать (при ее выборе) вкладку.*/
 	void activeTab();
+	/**Деактивировать вкладку.*/
 	void deActiveTab();
+	/**Закрыть/удалить вкладку.*/
 	void closeTab();
-	
-	UniformSystemPlugin getModel();
 }
