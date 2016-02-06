b8b15a3a79933bc3345e994cdd00ad16ae8b6362
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-07 00:11:57 +0300

    Add comment.

--- src/main/java/ru/kolaer/client/javafx/mvp/presenter/PWindowPlugin.java
+++ src/main/java/ru/kolaer/client/javafx/mvp/presenter/PWindowPlugin.java
@@ -4,8 +4,17 @@ import ru.kolaer.client.javafx.mvp.viewmodel.ExplorerWindowsObresvable;
 import ru.kolaer.client.javafx.mvp.viewmodel.VMLabel;
 import ru.kolaer.client.javafx.plugins.UniformSystemPlugin;
 
+/**
+ * Presenter для окна плагина.
+ * @author Danilov
+ * @version 0.1
+ * @deprecated Замена на {@linkplain PTab}.
+ */
 public interface PWindowPlugin extends ExplorerWindowsObresvable {
+	/**Получить ярлык плагина.*/
 	VMLabel getVMLabel();
+	/**Получить пресентер окна.*/
 	PWindow getWindow();
+	/**Получить плагин.*/
 	UniformSystemPlugin getPlugin();
 }
