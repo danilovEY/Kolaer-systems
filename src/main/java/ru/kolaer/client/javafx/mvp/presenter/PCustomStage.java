062f63ca0b6104ea2c6356cb26a89b936169e85c
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-06 23:51:05 +0300

    Add comment.

--- src/main/java/ru/kolaer/client/javafx/mvp/presenter/PCustomStage.java
+++ src/main/java/ru/kolaer/client/javafx/mvp/presenter/PCustomStage.java
@@ -7,7 +7,9 @@ import ru.kolaer.client.javafx.mvp.view.VCustomStage;
  * @author danilovey
  *
  */
-public interface PCustomStage extends PWindow{
+public interface PCustomStage extends PWindow {
+	/**Получить view окна.*/
 	VCustomStage getView();
+	/**Задать view окна.*/
 	void setView(final VCustomStage view);
 }
