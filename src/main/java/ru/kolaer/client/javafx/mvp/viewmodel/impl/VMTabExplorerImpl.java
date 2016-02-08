d82eb8644bbcf6072616be973df6a3b2ff1a85f1
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-08 09:44:38 +0300

    Refactoring.

--- src/main/java/ru/kolaer/client/javafx/mvp/viewmodel/impl/VMTabExplorerImpl.java
+++ src/main/java/ru/kolaer/client/javafx/mvp/viewmodel/impl/VMTabExplorerImpl.java
@@ -17,7 +17,6 @@ import org.slf4j.LoggerFactory;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TabPane;
 import ru.kolaer.client.javafx.mvp.presenter.PTab;
@@ -32,27 +31,34 @@ import ru.kolaer.client.javafx.services.ServiceControlManager;
 import ru.kolaer.client.javafx.system.UniformSystemEditorKit;
 import ru.kolaer.client.javafx.tools.Resources;
 
+/**
+ * Реализация вкладочного explorer'а.
+ *
+ * @author danilovey
+ * @version 0.1
+ */
 public class VMTabExplorerImpl extends  LoadFXML implements VTabExplorer, ExplorerObresvable {
 	private final Logger LOG = LoggerFactory.getLogger(VMTabExplorerImpl.class);
+	/**Вкладочная панель.*/
 	@FXML
 	private TabPane pluginsTabPane;
-	@FXML
-	private Menu fileMenu;
-	@FXML
-	private MenuItem exitMenuItem;
+	/**Менеджер служб.*/
 	private final ServiceControlManager servicesManager;
-	private final UniformSystemEditorKit editorKid;
+	/**Системные инструменты.*/
+	private final UniformSystemEditorKit editorKit;
+	/**Ключ - Имя вкладки, значение - Presenter вкладки.*/
 	private Map<String, PTab> pluginMap = new HashMap<>();
+	/**Коллекция обсерверов.*/
 	private List<ExplorerObserver> observers = new LinkedList<>();
 	
 	public VMTabExplorerImpl(final ServiceControlManager servicesManager, final UniformSystemEditorKit editorKid) {
 		super(Resources.V_TAB_EXPLORER);
 		this.servicesManager = servicesManager;
-		this.editorKid = editorKid;
+		this.editorKit = editorKid;
 	}
 
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+	public void initialize(final URL location, final ResourceBundle resources) {
 		this.initSelectionModel();
 	}
 
@@ -63,76 +69,77 @@ public class VMTabExplorerImpl extends  LoadFXML implements VTabExplorer, Explor
 
 	@Override
 	public void addPlugin(final UniformSystemPlugin plugin, final URLClassLoader jarClassLoaser) {
-		final ExecutorService threadFroLoadPlug = Executors.newSingleThreadExecutor();
 		CompletableFuture.supplyAsync(() -> {
 			Thread.currentThread().setContextClassLoader(jarClassLoaser);
 			Thread.currentThread().setName("Инициализация плангина "+plugin.getName()+" в виде вкладки");
 			try {
-				plugin.initialization(this.editorKid);
+				plugin.initialization(this.editorKit);
 				if(plugin.getServices() != null) {
-					Thread.currentThread().setName("Запуск служб из плагина: " + plugin.getName());
+					Thread.currentThread().setName("Добавление служб из плагина: " + plugin.getName());
 					plugin.getServices().parallelStream().forEach(this.servicesManager::addService);
 				}
-			} catch (Exception e) {
+			} catch (final Exception e) {
 				LOG.error("Ошибка при инициализации плагина: {}", plugin.getName(), e);
+				this.editorKit.getUISystemUS().getDialog().showErrorDialog(Thread.currentThread().getName(), "Ошибка при инициализации плагина!");
+				return null;
 			}
 			return plugin;
-		}, threadFroLoadPlug)
-		.exceptionally(t -> {
+		}).exceptionally(t -> {
 			LOG.error("Ошибка при инициализации плагина");
 			return null;
 		}).thenApplyAsync((plg) -> {
-			final PTab tabPlugin = new PTabImpl(jarClassLoaser, plg, this.editorKid);
-			this.pluginMap.put(plg.getApplication().getName(), tabPlugin);
-			return tabPlugin;
-		}, threadFroLoadPlug).exceptionally(t -> {
+			if(plg != null) {
+				final PTab tabPlugin = new PTabImpl(jarClassLoaser, plg, this.editorKit);
+				this.pluginMap.put(plg.getApplication().getName(), tabPlugin);
+				return tabPlugin;
+			} else {
+				return null;
+			}
+		}).exceptionally(t -> {
 			LOG.error("Ошибка при создании вкладки плагина");
 			return null;
 		}).thenAcceptAsync((tab) -> {
-			Platform.runLater(() -> {
-				//Для того, чтобы АСУП открывался первым
-				if(tab.getModel().getName().equals("ASUP")) {
-					this.pluginsTabPane.getTabs().add(0, tab.getView().getContent());
-					this.pluginsTabPane.getSelectionModel().selectFirst();
-				} else {
-					this.pluginsTabPane.getTabs().add(tab.getView().getContent());
-				}
-				this.notifyAddPlugin(tab);
-				threadFroLoadPlug.shutdown();
-			});
-		}, threadFroLoadPlug).exceptionally(t -> {
+			if(tab != null) {
+				Platform.runLater(() -> {
+					//Для того, чтобы АСУП открывался первым (приказ начальника)
+					if(tab.getModel().getName().equals("ASUP")) {
+						this.pluginsTabPane.getTabs().add(0, tab.getView().getContent());
+						this.pluginsTabPane.getSelectionModel().selectFirst();
+					} else {
+						this.pluginsTabPane.getTabs().add(tab.getView().getContent());
+					}
+					this.notifyAddPlugin(tab);
+				});
+			}
+		}).exceptionally(t -> {
 			LOG.error("Ошибка при добавлении плагина");
 			return null;
 		});	
 	}
-	
+	/**Инициализация модели выбора вкладки.*/
 	private void initSelectionModel() {
 		this.pluginsTabPane.getSelectionModel().selectedItemProperty().addListener((observer, oldTab, newTab)  -> {
-			if(oldTab != null) {
-				final ExecutorService treadDesActTab = Executors.newSingleThreadExecutor();				
+			if(oldTab != null) {		
 				CompletableFuture.runAsync(() -> {	
 					final PTab tab = this.pluginMap.get(oldTab.getText());
 					tab.deActiveTab();
 					this.notifyDeactivationPlugin(tab);
-				}, treadDesActTab);			
-				treadDesActTab.shutdown();
+				});			
 			}
 			
 			if(newTab != null) {
-				final ExecutorService treadActTab = Executors.newSingleThreadExecutor();			
 				CompletableFuture.runAsync(() -> {
 					final PTab tab = this.pluginMap.get(newTab.getText());
 					tab.activeTab();
 					this.notifyActivationPlugin(tab);
-				}, treadActTab);			
-				treadActTab.shutdown();
+				});
 			}
 		});
 	}
 	
 	@Override
 	public void removePlugin(UniformSystemPlugin plugin) {
-		// TODO Auto-generated method stub
+		// TODO Доделать удаление плагина
 		
 	}
 
