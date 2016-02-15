91beb268213899bd46427caaac12bd33cfa8ef31
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-15 10:57:58 +0300

    Remove old class.

--- src/main/java/ru/kolaer/client/javafx/system/KolaerDataBaseRESTful.java
+++ src/main/java/ru/kolaer/client/javafx/system/KolaerDataBaseRESTful.java
@@ -7,14 +7,8 @@ package ru.kolaer.client.javafx.system;
  * @version 0.1
  */
 public class KolaerDataBaseRESTful implements KolaerDataBase {
-	private final User1cDataBase user1cDataBase = new User1cDataBaseRESTful();
 	private final UserDataAllDataBase dataAllDataBase = new UserDataAllDataBaseRESTful();
 	private final UserBirthdayAllDataBase userBirthdayAllDataBase = new UserBirthdayAllDataBaseImpl();
-	
-	@Override
-	public User1cDataBase getUser1cDataBase() {
-		return this.user1cDataBase;
-	}
 
 	@Override
 	public UserDataAllDataBase getUserDataAllDataBase() {
