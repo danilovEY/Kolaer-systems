4acd961ff86468b9f6a96a8501ed70908611e337
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-15 14:07:07 +0300

    Remove old class.

--- src/main/java/ru/kolaer/client/javafx/system/KolaerDataBase.java
+++ src/main/java/ru/kolaer/client/javafx/system/KolaerDataBase.java
@@ -7,8 +7,6 @@ package ru.kolaer.client.javafx.system;
  * @version 0.1
  */
 public interface KolaerDataBase {
-	/**Получить объект для работы с таблицой User1C.*/
-	User1cDataBase getUser1cDataBase();
 	/**Получить объект для работы с таблицой DataAll.*/
 	UserDataAllDataBase getUserDataAllDataBase(); 
 	/**Получить объект для работы с таблицой BirthdayAll.*/
