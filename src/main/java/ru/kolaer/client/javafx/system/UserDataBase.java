0c306520de17adcda066df492b7ffa9368c9efdb
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-20 13:24:04 +0300

    Add getting user by names.

--- src/main/java/ru/kolaer/client/javafx/system/UserDataBase.java
+++ src/main/java/ru/kolaer/client/javafx/system/UserDataBase.java
@@ -8,5 +8,6 @@ public interface UserDataBase<T> {
 	T[] getUsersByBirthday(Date date);
 	T[] getUsersByRengeBirthday(Date dateBegin, Date dateEnd);
 	T[] getUsersBirthdayToday();
+	T[] getUsersByInitials(String initials);
 	int getCountUsersBirthday(Date date);
 }
