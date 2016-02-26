0dcd3ac277e284a27b73c13fbadbd87480d50e50
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-26 14:36:50 +0300

    Add getting all holidays.

--- src/main/java/ru/kolaer/client/javafx/system/PublicHolidaysDateBase.java
+++ src/main/java/ru/kolaer/client/javafx/system/PublicHolidaysDateBase.java
@@ -5,4 +5,5 @@ import ru.kolaer.server.dao.entities.PublicHolidays;
 public interface PublicHolidaysDateBase {
 	PublicHolidays[] getPublicHolidaysInThisMonth();
 	PublicHolidays[] getPublicHolidays(int month, int year);
+	PublicHolidays[] getPublicHolidaysAll();
 }
