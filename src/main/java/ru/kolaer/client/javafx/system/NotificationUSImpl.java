638ff1820be5886deccbdfeeec493612416dd0fb
Danilov Eugene <daniloveugene@yandex.ru>
2016-02-29 09:31:17 +0300

    Add position to notify.

--- src/main/java/ru/kolaer/client/javafx/system/NotificationUSImpl.java
+++ src/main/java/ru/kolaer/client/javafx/system/NotificationUSImpl.java
@@ -4,6 +4,7 @@ import org.controlsfx.control.Notifications;
 import org.controlsfx.control.action.Action;
 
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
@@ -28,25 +29,16 @@ public class NotificationUSImpl implements NotificationUS {
 	
 	@Override
 	public void showSimpleNotify(final String title, final String text, Duration duration, final NotifyAction... actions) {
-		Platform.runLater(() -> {
-			final Notifications notify = this.addActions(Notifications.create(), actions);
-			notify.hideAfter(duration);	
-			if(title != null)
-				notify.title(title);
-			if(text != null) {
-				notify.text(text);
-			} 	
-			notify.show();
-		});
+		this.showSimpleNotify(title, text, duration, Pos.BOTTOM_RIGHT, actions);
 	}
 
 	@Override
-	public void showErrorNotify(String title, String text) {	
+	public void showErrorNotify(final String title, final String text) {	
 		this.showErrorNotify(title, text, new NotifyAction[0]);
 	}
 	
 	@Override
-	public void showErrorNotify(String title, String text, final NotifyAction... actions) {	
+	public void showErrorNotify(final String title, final String text, final NotifyAction... actions) {	
 		Platform.runLater(() -> {
 			final Notifications notify = this.addActions(Notifications.create(), actions);
 			notify.hideAfter(Duration.seconds(15));
@@ -60,12 +52,12 @@ public class NotificationUSImpl implements NotificationUS {
 	}
 
 	@Override
-	public void showWarningNotify(String title, String text) {		
+	public void showWarningNotify(final String title, final String text) {		
 		this.showWarningNotify(title, text, new NotifyAction[0]);
 	}
 	
 	@Override
-	public void showWarningNotify(String title, String text, final NotifyAction... actions) {		
+	public void showWarningNotify(final String title, final String text, final NotifyAction... actions) {		
 		Platform.runLater(() -> {
 			final Notifications notify = this.addActions(Notifications.create(), actions);
 			notify.hideAfter(Duration.seconds(10));
@@ -79,27 +71,18 @@ public class NotificationUSImpl implements NotificationUS {
 	}
 
 	@Override
-	public void showInformationNotify(String title, String text) {
+	public void showInformationNotify(final String title, final String text) {
 		this.showInformationNotify(title, text, Duration.seconds(5));
 	}
 
 	@Override
-	public void showInformationNotify(String title, String text, Duration duration) {
+	public void showInformationNotify(final String title, final String text, final Duration duration) {
 		this.showInformationNotify(title, text, duration, new NotifyAction[0]);
 	}
 	
 	@Override
 	public void showInformationNotify(final String title, final String text, final Duration duration, final NotifyAction... actions) {
-		Platform.runLater(() -> {
-			final Notifications notify = this.addActions(Notifications.create(), actions);
-			notify.hideAfter(duration);
-			if(title != null)
-				notify.title(title);
-			if(text != null) {
-				notify.text(text);
-			}
-			notify.showInformation();
-		});
+		this.showInformationNotify(title, text, duration, Pos.BASELINE_RIGHT, actions);
 	}
 	
 	private synchronized Notifications addActions(final Notifications notify, final NotifyAction... actions) {
@@ -120,5 +103,35 @@ public class NotificationUSImpl implements NotificationUS {
 			notify.graphic(vBox);
 		}	
 		return notify;
+	}
+
+	@Override
+	public void showSimpleNotify(final String title, final  String text, final Duration duration, final Pos pos, final NotifyAction... actions) {
+		Platform.runLater(() -> {
+			final Notifications notify = this.addActions(Notifications.create(), actions);
+			notify.hideAfter(duration);	
+			notify.position(pos);
+			if(title != null)
+				notify.title(title);
+			if(text != null) {
+				notify.text(text);
+			} 	
+			notify.show();
+		});
+	}
+
+	@Override
+	public void showInformationNotify(final String title, final String text, final Duration duration, final Pos pos, final NotifyAction... actions) {
+		Platform.runLater(() -> {
+			final Notifications notify = this.addActions(Notifications.create(), actions);
+			notify.hideAfter(duration);
+			notify.position(pos);
+			if(title != null)
+				notify.title(title);
+			if(text != null) {
+				notify.text(text);
+			}
+			notify.showInformation();
+		});
 	}
 }
