69b61231ee1b4913f054a3fc2dece24525a1a3de
Danilov <daniloveugene@yandex.ru>
2016-04-20 15:16:23 +0300

    Add Tools class.

--- src/main/java/ru/kolaer/api/system/DialogUS.java
+++ src/main/java/ru/kolaer/api/system/DialogUS.java
@@ -6,11 +6,11 @@ import ru.kolaer.api.mvp.presenter.PDialog;
 /**Интерфейс для вызова диалоговых окон.*/
 public interface DialogUS {
 	/**Показать простой диалоговое окно.*/
-	PDialog showSimpleDialog(String title, String text);
+	PDialog createSimpleDialog(String title, String text);
 	/**Показать диалоговое окно информирующая об ошибке.*/
-	PDialog showErrorDialog(String title, String text);
+	PDialog createErrorDialog(String title, String text);
 	/**Показать диалоговое информирующее окно.*/
-	PDialog showInfoDialog(String title, String text);
+	PDialog createInfoDialog(String title, String text);
 	/**Показать диалоговое окно в progress bar'ом.*/
-	PDialog showLoadingDialog(Service<?> service);
+	PDialog createLoadingDialog(Service<?> service);
 }
