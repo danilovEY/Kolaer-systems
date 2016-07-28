d40e93edabb66d50f5eb42a15de744daffdcd075
Danilov <daniloveugene@yandex.ru>
2016-07-28 23:49:31 +0300

    Refactoring api.

---
+++ client/usa/src/main/java/ru/kolaer/client/javafx/system/KolaerWebServerImpl.java
@@ -0,0 +1,15 @@
+package ru.kolaer.client.javafx.system;
+
+import ru.kolaer.api.system.KolaerWebServer;
+import ru.kolaer.api.system.ServerStatus;
+
+/**
+ * Created by Danilov on 28.07.2016.
+ */
+public class KolaerWebServerImpl implements KolaerWebServer {
+
+    @Override
+    public ServerStatus getServerStatus() {
+        return null;
+    }
+}
