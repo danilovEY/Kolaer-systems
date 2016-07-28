07e3993423f77b65e3160c1c33370243486dab65
Danilov Eugene <daniloveugene@yandex.ru>
2016-07-28 11:23:21 +0300

    Add url path controller.

---
+++ server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/UrlPathController.java
@@ -0,0 +1,28 @@
+package ru.kolaer.server.webportal.mvc.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kolaer.server.webportal.mvc.model.dao.UrlPathDao;
+import ru.kolaer.server.webportal.mvc.model.entities.webportal.WebPortalUrlPath;
+
+import java.util.List;
+
+/**
+ * Created by danilovey on 28.07.2016.
+ */
+@RestController
+@RequestMapping("/api")
+public class UrlPathController {
+
+    @Autowired
+    private UrlPathDao urlPathDao;
+
+    @RequestMapping(value = "/get/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<WebPortalUrlPath> getAllUrl() {
+        return urlPathDao.findAll();
+    }
+
+}
