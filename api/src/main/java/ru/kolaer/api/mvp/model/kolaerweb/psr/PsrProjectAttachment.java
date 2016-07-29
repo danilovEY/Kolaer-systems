e690352355861cbf83e8eb7a31736e7c1ff09028
Danilov Eugene <daniloveugene@yandex.ru>
2016-07-29 10:54:08 +0300

    Divided psr attach to interface and base impl.

--- api/src/main/java/ru/kolaer/api/mvp/model/kolaerweb/psr/PsrProjectAttachment.java
+++ api/src/main/java/ru/kolaer/api/mvp/model/kolaerweb/psr/PsrProjectAttachment.java
@@ -3,23 +3,10 @@ package ru.kolaer.api.mvp.model.kolaerweb.psr;
 /**
  * Created by danilovey on 29.07.2016.
  */
-public class PsrProjectAttachment {
-    private int id;
-    private String pathFile;
+public interface PsrProjectAttachment {
+    int getId();
+    void setId(int id);
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPathFile() {
-        return pathFile;
-    }
-
-    public void setPathFile(String pathFile) {
-        this.pathFile = pathFile;
-    }
+    String getPathFile();
+    void setPathFile(String pathFile);
 }
