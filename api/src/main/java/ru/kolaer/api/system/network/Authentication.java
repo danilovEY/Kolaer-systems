003d7d7f04cb9e613048fb06f085199f0d019b3f
Danilov Eugene <daniloveugene@yandex.ru>
2016-08-02 15:20:12 +0300

    Edit interface to api for authentication.

--- api/src/main/java/ru/kolaer/api/system/network/Authentication.java
+++ api/src/main/java/ru/kolaer/api/system/network/Authentication.java
@@ -1,13 +1,14 @@
 package ru.kolaer.api.system.network;
 
-import ru.kolaer.api.mvp.model.kolaerweb.TokenJson;
+import ru.kolaer.api.mvp.model.kolaerweb.GeneralAccountsEntity;
 import ru.kolaer.api.mvp.model.kolaerweb.UserAndPassJson;
 
 /**
  * Created by danilovey on 02.08.2016.
  */
 public interface Authentication {
-    TokenJson login(UserAndPassJson userAndPassJson);
+    boolean login(UserAndPassJson userAndPassJson);
+    GeneralAccountsEntity getAuthorizedUser();
     boolean isAuthentication();
     boolean logout();
 }
