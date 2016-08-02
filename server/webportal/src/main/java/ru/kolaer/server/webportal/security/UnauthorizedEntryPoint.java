e7ce609950f4f4a9a111e688a5e0906c5714f930
Danilov Eugene <daniloveugene@yandex.ru>
2016-08-02 11:30:23 +0300

    Remove copyright :D

--- server/webportal/src/main/java/ru/kolaer/server/webportal/security/UnauthorizedEntryPoint.java
+++ server/webportal/src/main/java/ru/kolaer/server/webportal/security/UnauthorizedEntryPoint.java
@@ -8,24 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-/**
- * {@link AuthenticationEntryPoint} that rejects all requests with an unauthorized error message.
- *
- * @author Philip W. Sorst <philip@sorst.net>
- */
-public class UnauthorizedEntryPoint implements AuthenticationEntryPoint
-{
-
+public class UnauthorizedEntryPoint implements AuthenticationEntryPoint {
 	@Override
-	public void commence(
-			HttpServletRequest request,
-			HttpServletResponse response,
-			AuthenticationException authException) throws IOException, ServletException
-	{
-		response.sendError(
-				HttpServletResponse.SC_UNAUTHORIZED,
-				"Unauthorized: Authentication token was either missing or invalid.");
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Вы не авторизовались!");
 	}
 
 }
