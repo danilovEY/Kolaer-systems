db8a31f1648fdb5d24c146c9496b10c8af5230f0
Danilov Eugene <daniloveugene@yandex.ru>
2016-08-08 09:39:33 +0300

    Add convert LocalDate to Date.

--- api/src/main/java/ru/kolaer/api/tools/Tools.java
+++ api/src/main/java/ru/kolaer/api/tools/Tools.java
@@ -4,11 +4,18 @@ import javafx.application.Platform;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.Objects;
 import java.util.concurrent.*;
 
 public class Tools {
     private static final Logger LOG = LoggerFactory.getLogger(Tools.class);
+
+    public static Date convertToDate(final LocalDate date) {
+        return Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
 
     public static void runOnThreadFXAndWain(final Runnable runnable, final long time, final TimeUnit timeUnit) {
         Objects.requireNonNull(runnable, "runnable");
