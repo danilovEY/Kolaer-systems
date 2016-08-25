4c3743d4eb9d1dd7250789600d17e43e9966e09f
Danilov Eugene <daniloveugene@yandex.ru>
2016-08-25 11:28:37 +0300

    Bugfix.

--- server/restful/src/main/java/ru/kolaer/server/dao/impl/JdbcDbDataAllDaoImpl.java
+++ server/restful/src/main/java/ru/kolaer/server/dao/impl/JdbcDbDataAllDaoImpl.java
@@ -34,22 +34,22 @@ public class JdbcDbDataAllDaoImpl implements DbDataAllDAO {
 
     @Override
     public List<DbDataAll> getUserRangeBirthday(Date startData, Date endData) {
-        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone FROM db_data_all WHERE birthday BETWEEN ? AND ?", new Object[]{startData,endData}, (rs, row) -> this.createDbDataAll(rs));
+        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone, gender FROM db_data_all WHERE birthday BETWEEN ? AND ?", new Object[]{startData,endData}, (rs, row) -> this.createDbDataAll(rs));
     }
 
     @Override
     public List<DbDataAll> getUsersByBirthday(Date date) {
-        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone FROM db_data_all WHERE day(birthday) = day(?) and month(birthday) = month(?)", new Object[]{date}, (rs, row) -> this.createDbDataAll(rs));
+        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone, gender FROM db_data_all WHERE day(birthday) = day(?) and month(birthday) = month(?)", new Object[]{date, date}, (rs, row) -> this.createDbDataAll(rs));
     }
 
     @Override
     public List<DbDataAll> getUserBirthdayToday() {
-        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone FROM db_data_all WHERE day(birthday) = day(CURRENT_DATE) and month(birthday) = month(CURRENT_DATE)", (rs, row) -> this.createDbDataAll(rs));
+        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone, gender FROM db_data_all WHERE day(birthday) = day(CURRENT_DATE) and month(birthday) = month(CURRENT_DATE)", (rs, row) -> this.createDbDataAll(rs));
     }
 
     @Override
     public List<DbDataAll> getUsersByInitials(String initials) {
-        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone FROM db_data_all WHERE initials = ?", new Object[]{initials}, (rs, row) -> this.createDbDataAll(rs));
+        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone, gender FROM db_data_all WHERE initials = ?", new Object[]{initials}, (rs, row) -> this.createDbDataAll(rs));
     }
 
     @Override
@@ -59,12 +59,12 @@ public class JdbcDbDataAllDaoImpl implements DbDataAllDAO {
 
     @Override
     public List<DbDataAll> getAll() {
-        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone FROM db_data_all", (rs, row) -> this.createDbDataAll(rs));
+        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone, gender FROM db_data_all", (rs, row) -> this.createDbDataAll(rs));
     }
 
     @Override
     public List<DbDataAll> getAllMaxCount(int count) {
-        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone FROM db_data_all LIMIT " + count, (rs, row) -> this.createDbDataAll(rs));
+        return this.jdbcTemplate.query("SELECT person_number, initials, departament, post, email, vCard, birthday, category_unit, login, password, mobile_phone, phone, gender FROM db_data_all LIMIT " + count, (rs, row) -> this.createDbDataAll(rs));
     }
 
     @Override
@@ -84,6 +84,7 @@ public class JdbcDbDataAllDaoImpl implements DbDataAllDAO {
         dbDataAll.setDepartament(rs.getString("departament"));
         dbDataAll.setPost(rs.getString("post"));
         dbDataAll.setPhone(rs.getString("phone"));
+        dbDataAll.setGender(rs.getString("gender"));
         dbDataAll.setMobilePhone(rs.getString("mobile_phone"));
         dbDataAll.setEmail(rs.getString("email"));
         dbDataAll.setVCard(rs.getString("vCard"));
