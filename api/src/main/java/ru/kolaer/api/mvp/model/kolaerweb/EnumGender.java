bfc2799f19f4941c0141aa93c5d008fb113250b0
Danilov Eugene <daniloveugene@yandex.ru>
2016-09-12 15:09:33 +0300

    Add toString Enum.

--- api/src/main/java/ru/kolaer/api/mvp/model/kolaerweb/EnumGender.java
+++ api/src/main/java/ru/kolaer/api/mvp/model/kolaerweb/EnumGender.java
@@ -4,5 +4,16 @@ package ru.kolaer.api.mvp.model.kolaerweb;
  * Created by Danilov on 27.07.2016.
  */
 public enum EnumGender {
-    MALE, FEMALE
+    MALE("Мужской"), FEMALE("Женский");
+
+    private String name;
+
+    EnumGender(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
