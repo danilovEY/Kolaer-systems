d58f5ff00054ed6e54762af567438099e89cf4ff
Danilov Eugene <daniloveugene@yandex.ru>
2016-09-13 08:20:05 +0300

    Add getting time to json.

--- server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/ToolsController.java
+++ server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/ToolsController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kolaer.api.mvp.model.kolaerweb.DateTimeJson;
+import ru.kolaer.server.webportal.annotations.UrlDeclaration;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
@@ -17,6 +19,7 @@ import java.time.format.DateTimeFormatter;
 @RequestMapping("/non-security/tools")
 public class ToolsController {
 
+    @UrlDeclaration(description = "Получить серверное время",isAccessAll = true)
     @RequestMapping(value = "get/time", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public DateTimeJson getTime() {
         final DateTimeJson dateTimeJson = new DateTimeJson();
@@ -27,4 +30,9 @@ public class ToolsController {
         return dateTimeJson;
     }
 
+    @UrlDeclaration(description = "Получить подробное серверное время",isAccessAll = true)
+    @RequestMapping(value = "get/time/default", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public LocalDateTime getDefaultTime() {
+        return LocalDateTime.now();
+    }
 }
