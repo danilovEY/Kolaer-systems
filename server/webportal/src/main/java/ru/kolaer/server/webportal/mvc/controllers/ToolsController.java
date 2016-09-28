5f6edc4bec904ba9a3157aea442dd65ea1ffbbd1
Danilov Eugene <daniloveugene@yandex.ru>
2016-09-28 10:57:58 +0300

    Add getting period.

--- server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/ToolsController.java
+++ server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/ToolsController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kolaer.api.mvp.model.kolaerweb.DateTimeJson;
+import ru.kolaer.api.tools.Tools;
 import ru.kolaer.server.webportal.annotations.UrlDeclaration;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Created by danilovey on 25.08.2016.
@@ -28,6 +31,44 @@ public class ToolsController {
         dateTimeJson.setDate(localDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
         dateTimeJson.setTime(localTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
         return dateTimeJson;
+    }
+
+    @UrlDeclaration(description = "Получить период до времени",isAccessAll = true)
+    @RequestMapping(value = "get/period", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public DateTimeJson getPeriod(DateTimeJson dateTimeJson) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse(dateTimeJson.getDate()+" "+dateTimeJson.getTime(), formatter);
+        LocalDateTime tempDateTime = LocalDateTime.now();
+
+        long years = tempDateTime.until(dateTime, ChronoUnit.YEARS);
+        tempDateTime = tempDateTime.plusYears(years);
+
+        long months = tempDateTime.until(dateTime, ChronoUnit.MONTHS);
+        tempDateTime = tempDateTime.plusMonths(months);
+
+        long days = tempDateTime.until(dateTime, ChronoUnit.DAYS);
+        tempDateTime = tempDateTime.plusDays(days);
+
+        long hours = tempDateTime.until(dateTime, ChronoUnit.HOURS);
+        tempDateTime = tempDateTime.plusHours(hours);
+
+        long minutes = tempDateTime.until(dateTime, ChronoUnit.MINUTES);
+        tempDateTime = tempDateTime.plusMinutes(minutes);
+
+        long seconds = tempDateTime.until(dateTime, ChronoUnit.SECONDS);
+
+        LocalDate localDate = LocalDate.of(Math.toIntExact(years),
+                Math.toIntExact(months),
+                Math.toIntExact(days));
+
+        LocalTime localTime = LocalTime.of(Math.toIntExact(hours),
+                Math.toIntExact(minutes),
+                Math.toIntExact(seconds));
+
+        final DateTimeJson result = new DateTimeJson();
+        result.setDate(localDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        result.setTime(localTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        return result;
     }
 
     @UrlDeclaration(description = "Получить подробное серверное время",isAccessAll = true)
