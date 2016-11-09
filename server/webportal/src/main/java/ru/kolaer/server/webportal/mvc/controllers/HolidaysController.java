76e4b8a2be59a6c380e10ce220e6a00b354196f7
Danilov Eugene <daniloveugene@yandex.ru>
2016-11-09 12:35:24 +0300

    Fix url descriptions.

--- server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/HolidaysController.java
+++ server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/HolidaysController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kolaer.api.mvp.model.kolaerweb.DateTimeJson;
 import ru.kolaer.api.mvp.model.kolaerweb.Holiday;
+import ru.kolaer.server.webportal.annotations.UrlDeclaration;
 import ru.kolaer.server.webportal.mvc.model.servirces.HolidayService;
 
 import java.util.List;
@@ -22,11 +23,13 @@ public class HolidaysController {
     @Autowired
     private HolidayService holidayService;
 
+    @UrlDeclaration(description = "Получить все праздники.", isAccessAll = true)
     @RequestMapping(value = "/get/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Holiday> getHolidaysAll() {
         return this.holidayService.getAllHolidays();
     }
 
+    @UrlDeclaration(description = "Получить праздники в месяце.", isAccessAll = true)
     @RequestMapping(value = "/get/{month}/{year}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Holiday> getPublicHolidays(@PathVariable final String month, @PathVariable final String year) {
         final DateTimeJson dateTimeJson = new DateTimeJson("01." + month + "." + year, "00:00:00");
