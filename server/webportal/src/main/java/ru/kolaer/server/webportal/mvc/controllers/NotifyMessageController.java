d0bfcd3d946997936b9ab3bdf536b6326e967170
Danilov Eugene <daniloveugene@yandex.ru>
2016-11-21 12:28:49 +0300

    Add swagger doc to notifications controller.

--- server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/NotifyMessageController.java
+++ server/webportal/src/main/java/ru/kolaer/server/webportal/mvc/controllers/NotifyMessageController.java
@@ -1,5 +1,8 @@
 package ru.kolaer.server.webportal.mvc.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,20 +19,29 @@ import ru.kolaer.server.webportal.mvc.model.servirces.NotifyMessageService;
  */
 @RestController
 @RequestMapping("/non-security/notify")
+@Api(tags = "Нотификация", description = "Пуш уведомления")
 public class NotifyMessageController {
 
     @Autowired
     private NotifyMessageService notifyMessageService;
 
+    @ApiOperation(
+            value = "Получить последнее оповещение",
+            notes = "Получить последнее оповещение"
+    )
     @UrlDeclaration(description = "Получить последнее оповещение.", isAccessAll = true)
     @RequestMapping(value = "/get/last", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public NotifyMessage getLastNotifyMessage() {
         return this.notifyMessageService.getLastNotifyMessage();
     }
 
+    @ApiOperation(
+            value = "Добавить оповещение",
+            notes = "Добавить оповещение"
+    )
     @UrlDeclaration(description = "Добавить оповещение.", requestMethod = RequestMethod.POST)
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public void addNotifyMessage(@RequestBody NotifyMessage notifyMessage) {
+    public void addNotifyMessage(@ApiParam(value = "Сообщение", required = true) @RequestBody NotifyMessage notifyMessage) {
         notifyMessage.setId(1);
         this.notifyMessageService.update(new NotifyMessageDecorator(notifyMessage));
     }
