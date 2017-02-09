3d8068116d22bb53de16b3d56a2e72c349e4170c
Danilov Eugene <daniloveugene@yandex.ru>
2017-02-09 08:44:17 +0300

    Initialize kolpass plugin.

---
+++ client/kolpass/src/main/java/ru/kolaer/kolpass/mvp/model/dto/RepositoryPasswordDto.java
@@ -0,0 +1,10 @@
+package ru.kolaer.kolpass.mvp.model.dto;
+
+import lombok.Data;
+
+/**
+ * Created by danilovey on 09.02.2017.
+ */
+@Data
+public class RepositoryPasswordDto {
+}
---
+++ client/kolpass/src/main/java/ru/kolaer/kolpass/runnable/KolpassPlugin.java
@@ -0,0 +1,60 @@
+package ru.kolaer.kolpass.runnable;
+
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import lombok.extern.slf4j.Slf4j;
+import ru.kolaer.api.plugins.UniformSystemPlugin;
+import ru.kolaer.api.plugins.services.Service;
+import ru.kolaer.api.system.UniformSystemEditorKit;
+
+import java.net.URL;
+import java.util.Collection;
+
+/**
+ * Created by danilovey on 09.02.2017.
+ */
+@Slf4j
+public class KolpassPlugin implements UniformSystemPlugin {
+    private BorderPane mainPane;
+
+    @Override
+    public void initialization(UniformSystemEditorKit editorKit) throws Exception {
+        log.info("KOLPASS INITIALIZATION!");
+    }
+
+    @Override
+    public URL getIcon() {
+        return null;
+    }
+
+    @Override
+    public Collection<Service> getServices() {
+        return null;
+    }
+
+    @Override
+    public void start() throws Exception {
+        this.mainPane = new BorderPane(new Label("KolPass"));
+    }
+
+    @Override
+    public void stop() throws Exception {
+
+    }
+
+    @Override
+    public void updatePluginObjects(String key, Object object) {
+
+    }
+
+    @Override
+    public void setContent(Parent content) {
+
+    }
+
+    @Override
+    public Parent getContent() {
+        return this.mainPane;
+    }
+}
