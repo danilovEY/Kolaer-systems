2f705c0864ea007282bd09dc6d6ade05a1cbb1c6
Danilov Eugene <daniloveugene@yandex.ru>
2017-03-27 11:01:05 +0300

    Bugfix: when change event with keyboard.

--- client/wer/src/main/java/ru/kolaer/client/wer/mvp/view/VEventTableImpl.java
+++ client/wer/src/main/java/ru/kolaer/client/wer/mvp/view/VEventTableImpl.java
@@ -14,6 +14,7 @@ import ru.kolaer.client.wer.mvp.model.System;
 import java.text.SimpleDateFormat;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 /**
@@ -139,9 +140,9 @@ public class VEventTableImpl implements VEventTable {
 
     @Override
     public void setOnSelectEvent(Function<Event, Void> function) {
-        this.eventTableView.setOnMouseClicked(e -> {
-            function.apply(this.eventTableView.getSelectionModel().getSelectedItem());
-        });
+        this.eventTableView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) ->
+                Optional.ofNullable(newSelection).ifPresent(function::apply)
+        );
     }
 
     @Override
