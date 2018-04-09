e8e32043063ba4ac559c6414f8881a90fd8a76dc
Danilov Eugene <daniloveugene@yandex.ru>
2018-04-09 16:45:22 +0300

    Add info about user account.

--- web-client/src/app/commons/pages/setting/setting.component.ts
+++ web-client/src/app/commons/pages/setting/setting.component.ts
@@ -1,15 +1,20 @@
 import {Component, OnInit} from '@angular/core';
+import {AccountService} from '../../services/account.service';
+import {AccountModel} from '../../models/account.model';
 
 @Component({
-  selector: 'app-setting',
-  templateUrl: './setting.component.html',
-  styleUrls: ['./setting.component.scss']
+    selector: 'app-setting',
+    templateUrl: './setting.component.html',
+    styleUrls: ['./setting.component.scss']
 })
 export class SettingComponent implements OnInit {
+    currentAccount: AccountModel;
 
-  constructor() { }
+    constructor(private _accountService: AccountService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this._accountService.getCurrentAccount().subscribe(account => this.currentAccount = account );
+    }
 
 }
--- web-client/src/app/commons/pages/setting/setting.module.ts
+++ web-client/src/app/commons/pages/setting/setting.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {SettingComponent} from './setting.component';
 import {SettingRoutingModule} from './setting.routing';
 import {AppCoreModule} from '../../app-core.module';
+import {ActiveModule} from 'angular-bootstrap-md/index';
 
 @NgModule({
     imports: [
         AppCoreModule,
+        ActiveModule.forRoot(),
         SettingRoutingModule
     ],
     declarations: [
