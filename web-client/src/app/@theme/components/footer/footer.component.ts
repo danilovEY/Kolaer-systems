7fd13022611f40f87f245b9d9ab622b697309f29
Danilov Eugene <daniloveugene@yandex.ru>
2018-05-24 10:25:44 +0300

    Add version and edit menu items.

--- web-client/src/app/@theme/components/footer/footer.component.ts
+++ web-client/src/app/@theme/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
+import {environment} from '../../../../environments/environment';
 
 @Component({
-  selector: 'ngx-footer',
-  styleUrls: ['./footer.component.scss'],
-  template: `
-    <span class="created-by">ОИТ 2018</span>
-  `,
+    selector: 'ngx-footer',
+    styleUrls: ['./footer.component.scss'],
+    template: `
+        <span class="created-by">ОИТ 2018 (v{{version}})</span>
+    `,
 })
 export class FooterComponent {
+  readonly version: string = environment.version;
 }
--- web-client/src/app/pages/pages.component.ts
+++ web-client/src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import {MENU_ITEMS} from './pages-menu';
+import {NbMenuItem} from '@nebular/theme';
+import {AuthenticationRestService} from '../@core/modules/auth/authentication-rest.service';
 
 @Component({
     selector: 'ngx-pages',
@@ -11,7 +12,42 @@ import {MENU_ITEMS} from './pages-menu';
         </default-layout>
     `,
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+    menu: NbMenuItem[] = [];
 
-    menu = MENU_ITEMS;
+    constructor(private authService: AuthenticationRestService) {
+
+    }
+
+    ngOnInit() {
+        const dashboardMenuItem: NbMenuItem = new NbMenuItem();
+        dashboardMenuItem.title = 'Домашняя страница';
+        dashboardMenuItem.icon = 'nb-home';
+        dashboardMenuItem.link = '/pages/dashboard';
+        dashboardMenuItem.home = true;
+
+
+        const appMenuMenuItem: NbMenuItem = new NbMenuItem();
+        appMenuMenuItem.title = 'Приложения';
+        appMenuMenuItem.group = true;
+
+        this.menu.push(dashboardMenuItem, appMenuMenuItem);
+
+        if (this.authService.authentication) {
+            const kolpassMenuItem: NbMenuItem = new NbMenuItem();
+            kolpassMenuItem.title = 'Парольница';
+            kolpassMenuItem.icon = 'nb-locked';
+            kolpassMenuItem.children = [];
+
+            const kolpassMainMenuItem: NbMenuItem = new NbMenuItem();
+            kolpassMainMenuItem.title = 'Главная';
+            kolpassMainMenuItem.link = 'app/kolpass';
+
+            kolpassMenuItem.children.push(kolpassMainMenuItem);
+
+            this.menu.push(kolpassMenuItem);
+        }
+
+    }
+
 }
