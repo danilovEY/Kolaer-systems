8aa8d8b2edf5828e6151302df0e51284016c1b1f
Danilov Eugene <daniloveugene@yandex.ru>
2018-06-26 10:05:14 +0300

    Update nebular UI + fix bug ie11.

--- web-client/src/polyfills.ts
+++ web-client/src/polyfills.ts
@@ -1,7 +1,3 @@
-/***************************************************************************************************
- * BROWSER POLYFILLS
- */
-
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 import 'core-js/es6/symbol';
 import 'core-js/es6/object';
@@ -15,35 +11,28 @@ import 'core-js/es6/date';
 import 'core-js/es6/array';
 import 'core-js/es6/regexp';
 import 'core-js/es6/map';
-import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js'; // Run `npm install --save classlist.js`.
-/** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 import 'web-animations-js'; // Run `npm install --save web-animations-js`.
+/** Evergreen browsers require these. **/
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone'; // Included with Angular CLI.
 /**
- * By default, zone.js will patch all possible macroTask and DomEvents
- * user can disable parts of macroTask/DomEvents patch by setting following flags
- */
-(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
-(window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
-(window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
-
-/**
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-// import 'intl'; // Run `npm install --save intl`.
-// import 'intl/locale-data/jsonp/en';
-// import 'core-js/es7/array';
-// import 'core-js/es7/object';
+import 'core-js/es7/array';
+import 'core-js/es7/object';
+
+
+/** ALL Firefox browsers require the following to support `@angular/animation`. **/
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 /***************************************************************************************************
@@ -53,3 +42,4 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 if (typeof SVGElement.prototype.contains === 'undefined') {
   SVGElement.prototype.contains = HTMLDivElement.prototype.contains;
 }
+
